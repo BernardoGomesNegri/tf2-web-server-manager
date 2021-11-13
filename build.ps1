e384554fbbb02d84efd3a5f6cb6c2bed99bc7347
BernardoGomesNegri <38192358+BernardoGomesNegri@users.noreply.github.com>
2021-11-13 11:44:33 -0300

    now supports incremental builds

--- build.ps1
+++ build.ps1
@@ -1,6 +1,15 @@
 if (($args[0]) -eq "final")
 {
     Write-Host "With optimizations ON"
+    if (Test-Path -Path "./out/build")
+    {
+        # Assume the rest exists
+        # GHC may or may not force a rebuild if optimization changes, but I am not sure, so delete the cache just to be certain
+        Remove-Item "./out/build" -Recurse
+        Remove-Item "./out/cache" -Recurse
+        Remove-Item "./out/packagedb" -Recurse
+        Remove-Item "./out/tmp" -Recurse
+    }
     cabal build --builddir=./out
     $extraelmflags = "--optimize"
 }
@@ -26,11 +35,7 @@ if ($IsWindows)
 $exe_path = "./out/build/" + $system + "/ghc-" + $ghcver + "/tf2-server-manager-" + $version + "/x/tf2-server-manager/build/tf2-server-manager/" + $exe
 $out_exe = "./out/" + $exe
 Write-Host ("Copying: " + $exe_path)
-Move-Item $exe_path $out_exe -Force
-Remove-Item "./out/build" -Recurse
-Remove-Item "./out/cache" -Recurse
-Remove-Item "./out/packagedb" -Recurse
-Remove-Item "./out/tmp" -Recurse
+Copy-Item $exe_path $out_exe -Force
 cd frontend
 Get-ChildItem static | Copy-Item -Destination ../out/frontend -Recurse
 elm make src/Login.elm --output=../out/frontend/login.js $extraelmflags
